4fc4ec3804b525ff522539324f780b05bf7621b8
Динара Алдарова <dinara_aldarova@mail.ru>
2019-12-13 16:25:46 +0500

    Добавлено определение типа файла EXE

--- Program.cs
+++ Program.cs
@@ -18,36 +18,36 @@ namespace VirusHeuristicAnalize
                 string[] files = Directory.GetFiles(dirName);
                 foreach (string path in files)
                 {
-                    File file1 = new File(path);
-                    Console.WriteLine("{0} {1}", path, file1.IsInfected() ? "ЗАРАЖЕН" : "не заражен");
+                    File file = new File(path);
+                    Console.WriteLine("{0,-45} {1,-15} {2,-10}", path, file.IsInfected() ? "ЗАРАЖЕН" : "не заражен", file.TypeFile());
                 }
                 Console.ReadLine();
             }
 
-            //C: \Users\dinar\Desktop\WORK\BAD.ASM не заражен
-            //C:\Users\dinar\Desktop\WORK\BAD.COM ЗАРАЖЕН           //первый зараженный COM
-            //C: \Users\dinar\Desktop\WORK\BAD.LST не заражен
-            //C:\Users\dinar\Desktop\WORK\BAD.OBJ не заражен
-            //C:\Users\dinar\Desktop\WORK\DN.BAT не заражен
-            //C:\Users\dinar\Desktop\WORK\EXEPROC.exe ЗАРАЖЕН       //работа коллеги
-            //C: \Users\dinar\Desktop\WORK\GOOD.ASM не заражен
-            //C:\Users\dinar\Desktop\WORK\GOOD.COM не заражен
-            //C:\Users\dinar\Desktop\WORK\GOOD.EXE не заражен
-            //C:\Users\dinar\Desktop\WORK\GOOD.LST не заражен
-            //C:\Users\dinar\Desktop\WORK\GOOD.OBJ не заражен
-            //C:\Users\dinar\Desktop\WORK\GOOD_PA.BAK не заражен
-            //C:\Users\dinar\Desktop\WORK\GOOD_PA.EXE не заражен
-            //C:\Users\dinar\Desktop\WORK\GOOD_PA.PAS не заражен
-            //C:\Users\dinar\Desktop\WORK\MARAWI.COM ЗАРАЖЕН        //исследуемый вирус
-            //C: \Users\dinar\Desktop\WORK\sick.ASM не заражен
-            //C:\Users\dinar\Desktop\WORK\sick.COM ЗАРАЖЕН          //переписанный зараженный COM
-            //C: \Users\dinar\Desktop\WORK\sick.EXE ЗАРАЖЕН         //работающий зараженный EXE
-            //C: \Users\dinar\Desktop\WORK\sick.LST не заражен
-            //C:\Users\dinar\Desktop\WORK\sick.OBJ не заражен
-            //C:\Users\dinar\Desktop\WORK\sick_old.EXE ЗАРАЖЕН      //первая версия зараженного EXE
-            //C: \Users\dinar\Desktop\WORK\TASM.BAT не заражен
-            //C:\Users\dinar\Desktop\WORK\TD.BAT не заражен
-            //C:\Users\dinar\Desktop\WORK\UHEX.BAT не заражен
+            //C: \Users\dinar\Desktop\WORK\BAD.ASM      не заражен      undefined
+            //C: \Users\dinar\Desktop\WORK\BAD.COM      ЗАРАЖЕН         undefined       //первый зараженный COM
+            //C: \Users\dinar\Desktop\WORK\BAD.LST      не заражен      undefined
+            //C: \Users\dinar\Desktop\WORK\BAD.OBJ      не заражен      undefined
+            //C: \Users\dinar\Desktop\WORK\DN.BAT       не заражен      undefined
+            //C: \Users\dinar\Desktop\WORK\EXEPROC.exe  ЗАРАЖЕН         EXE - файл      //работа коллеги
+            //C: \Users\dinar\Desktop\WORK\GOOD.ASM     не заражен      undefined
+            //C: \Users\dinar\Desktop\WORK\GOOD.COM     не заражен      undefined
+            //C: \Users\dinar\Desktop\WORK\GOOD.EXE     не заражен      EXE-файл
+            //C: \Users\dinar\Desktop\WORK\GOOD.LST     не заражен      undefined
+            //C: \Users\dinar\Desktop\WORK\GOOD.OBJ     не заражен      undefined
+            //C: \Users\dinar\Desktop\WORK\GOOD_PA.BAK  не заражен      undefined
+            //C: \Users\dinar\Desktop\WORK\GOOD_PA.EXE  не заражен      EXE-файл
+            //C: \Users\dinar\Desktop\WORK\GOOD_PA.PAS  не заражен      undefined
+            //C: \Users\dinar\Desktop\WORK\MARAWI.COM   ЗАРАЖЕН         undefined       //исследуемый вирус
+            //C: \Users\dinar\Desktop\WORK\sick.ASM     не заражен      undefined
+            //C: \Users\dinar\Desktop\WORK\sick.COM     ЗАРАЖЕН         undefined       //переписанный зараженный COM
+            //C: \Users\dinar\Desktop\WORK\sick.EXE     ЗАРАЖЕН         EXE - файл      //работающий зараженный EXE
+            //C: \Users\dinar\Desktop\WORK\sick.LST     не заражен      undefined
+            //C: \Users\dinar\Desktop\WORK\sick.OBJ     не заражен      undefined
+            //C: \Users\dinar\Desktop\WORK\sick_old.EXE ЗАРАЖЕН         EXE - файл      //первая версия зараженного EXE
+            //C: \Users\dinar\Desktop\WORK\TASM.BAT     не заражен      undefined
+            //C: \Users\dinar\Desktop\WORK\TD.BAT       не заражен      undefined
+            //C: \Users\dinar\Desktop\WORK\UHEX.BAT     не заражен      undefined
         }
     }
 }
